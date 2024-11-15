2faab8508691444b32f49c82b814afcfdf6f43f7
yeom0325 <tjsfls95@gmail.com>
2024-11-16 02:41:14 +0900

    [#9]refactor:update App.tsx to integrate changes in startFilter file

--- MGFilterShow/src/App.tsx
+++ MGFilterShow/src/App.tsx
@@ -1,6 +1,9 @@
-import { useState, useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import './App.css';
 import Webcam from 'react-webcam';
+import { buttonData } from './data/buttonData';
+import TestPenaltyButton from './components/TestPenaltyButton';
+import { startFilter } from './utils/startFilter';
 
 const videoSize = {
     width: 640,
@@ -11,19 +14,39 @@ function App() {
     const webcamRef = useRef<Webcam>(null);
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const initialLoadedRef = useRef<boolean>(false);
+    const canvasContext = useRef<CanvasRenderingContext2D | null>(null);
+    const [isCanvasReady, setIsCanvasReady] = useState(false); // canvas 상태 관리
 
     useEffect(() => {
-        const canvasContext = canvasRef.current?.getContext('2d');
+        if (canvasRef.current) {
+            canvasContext.current = canvasRef.current.getContext('2d');
+        }
 
-        if (!canvasContext || initialLoadedRef.current) {return;}
+        if (!canvasContext.current || initialLoadedRef.current) {return;}
 
         initialLoadedRef.current = true;
+        setIsCanvasReady(true); // canvasContext가 설정되었음을 표시
+
+        // Webcam의 비디오 요소 가져오기
+        const video = webcamRef.current?.video;
+
+        startFilter(canvasContext.current,video);
     }, []);
 
     return (
         <main>
             <div className='test-button-container'>
-
+                {isCanvasReady && buttonData.map((button, index) => (
+                    <TestPenaltyButton
+                        key={index}
+                        buttonText={button.buttonText}
+                        filterPath={button.filterPath}
+                        canvas={canvasContext.current!}
+                        webcamRef={webcamRef}
+                        videoSize={videoSize}
+                        filterType={button.filterType}
+                    />
+                ))}
             </div>
             <div className="webcam-container">
                 <Webcam
@@ -38,7 +61,6 @@ function App() {
                     className="filter-canvas"
                 />
             </div>
-            <p className="status">{status}</p>
         </main>
     );
 }
